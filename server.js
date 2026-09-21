98a42057c8d45098c9392500f0f555245a70996e
Harshitha <harshitha.mandala1@gmail.com>
2026-09-21 14:43:00 +0530

    Initial Node.js REST API with Docker

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/status", (req, res) => {
+    res.json({
+        status: "success",
+        message: "Node.js API is running successfully"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
